56a3b42b4c8c08e5684652584ceb551f7dbfabc6
jb-holographik <jean-baptiste@holographik.co>
2025-06-25 17:35:36 +0200

    Re-push full project from local editor

--- vite.config.js
+++ vite.config.js
@@ -1,44 +1,39 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import eslintPlugin from 'vite-plugin-eslint'
 import fs from 'fs'
 import path from 'path'
 
-export default defineConfig(({ command }) => {
-  const common = {
-    plugins: [eslintPlugin({ cache: false })],
-    build: {
-      // désactive manifest s'il n'est pas nécessaire en CI
-      manifest: false,
-      rollupOptions: {
-        // CHEMIN ABSOLU vers ton point d'entrée
-        input: path.resolve(__dirname, 'src/main.js'),
-        output: {
-          entryFileNames: 'main.js',
-          format: 'iife',      // ou 'umd' selon ton besoin
-          compact: true,
-        },
-        external: ['jquery'],
-      },
+export default defineConfig({
+  plugins: [eslintPlugin({ cache: false })],
+  server: {
+    host: 'localhost',
+    cors: '*',
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, 'certs/localhost-key.pem')),
+      cert: fs.readFileSync(path.resolve(__dirname, 'certs/localhost.pem')),
     },
-  }
-
-  if (command === 'serve') {
-    return {
-      ...common,
-      server: {
-        host: 'localhost',
-        cors: '*',
-        // load certs **seulement** en dev local
-        https: {
-          key: fs.readFileSync(path.resolve(__dirname, 'certs/localhost-key.pem')),
-          cert: fs.readFileSync(path.resolve(__dirname, 'certs/localhost.pem')),
+    hmr: {
+      host: 'localhost',
+      protocol: 'wss', // wss = secure WebSocket
+    },
+  },
+  build: {
+    minify: true,
+    manifest: true,
+    rollupOptions: {
+      input: './src/main.js',
+      output: {
+        format: 'umd',
+        entryFileNames: 'main.js',
+        esModule: false,
+        compact: true,
+        globals: {
+          jquery: '$',
         },
-        hmr: { host: 'localhost', protocol: 'wss' },
+        manualChunks: undefined,
+        inlineDynamicImports: true,
       },
-    }
-  }
-
-  // build / preview en CI : pas de https
-  return common
+      external: ['jquery'],
+    },
+  },
 })
