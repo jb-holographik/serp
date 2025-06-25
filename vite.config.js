7b615a2b945000573b59412999dc2b7b866c99e6
jb-holographik <jean-baptiste@holographik.co>
2025-06-25 17:14:25 +0200

    bug fix

--- vite.config.js
+++ vite.config.js
@@ -3,37 +3,43 @@ import eslintPlugin from 'vite-plugin-eslint'
 import fs from 'fs'
 import path from 'path'
 
-export default defineConfig({
-  plugins: [eslintPlugin({ cache: false })],
-  server: {
-    host: 'localhost',
-    cors: '*',
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, 'certs/localhost-key.pem')),
-      cert: fs.readFileSync(path.resolve(__dirname, 'certs/localhost.pem')),
-    },
-    hmr: {
-      host: 'localhost',
-      protocol: 'wss', // wss = secure WebSocket
-    },
-  },
-  build: {
-    minify: true,
-    manifest: true,
-    rollupOptions: {
-      input: './src/main.js',
-      output: {
-        format: 'umd',
-        entryFileNames: 'main.js',
-        esModule: false,
-        compact: true,
-        globals: {
-          jquery: '$',
+export default defineConfig(({ command }) => {
+  const common = {
+    plugins: [eslintPlugin({ cache: false })],
+    build: {
+      minify: true,
+      manifest: true,
+      rollupOptions: {
+        input: './src/main.js',
+        output: {
+          format: 'umd',
+          entryFileNames: 'main.js',
+          esModule: false,
+          compact: true,
+          globals: { jquery: '$' },
+          inlineDynamicImports: true,
         },
-        manualChunks: undefined,
-        inlineDynamicImports: true,
+        external: ['jquery'],
       },
-      external: ['jquery'],
     },
-  },
+  }
+
+  if (command === 'serve') {
+    // mode dev local : on active HTTPS + HMR
+    return {
+      ...common,
+      server: {
+        host: 'localhost',
+        cors: '*',
+        https: {
+          key: fs.readFileSync(path.resolve(__dirname, 'certs/localhost-key.pem')),
+          cert: fs.readFileSync(path.resolve(__dirname, 'certs/localhost.pem')),
+        },
+        hmr: { host: 'localhost', protocol: 'wss' },
+      },
+    }
+  } else {
+    // mode build ou preview en CI : on ne touche qu'au build
+    return common
+  }
 })
